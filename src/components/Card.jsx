2450826910d451e613e745a5fa27216dfe247243
Sushovan <sushovanghosh960@gmail.com>
2024-02-07 13:50:02 +0530

    add star image and style optimization

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,4 @@
-import rateStar from '../utils/rating';
 import { ASSET_URL } from '../utils/data';
-
 
 export default function RestaurantCard({ props: { info } }) {
 
@@ -8,15 +6,20 @@ export default function RestaurantCard({ props: { info } }) {
         <>
             <div className='res-cards'>
                 <div>
-                    <img src={`${ASSET_URL}/${info.cloudinaryImageId}`} alt={info.cuisines.join(', ') + ' image'} />
+                    <img src={`${ASSET_URL}/${info.cloudinaryImageId}`} alt={info.cuisines.join(', ') + ' image'} className='food-image' />
                 </div>
 
                 <div className='card-details'>
-                    <h4>{info.name} 🏪</h4>
-                    <h3 style={{ color: '#ee5732' }}>😋</h3>
+                    <h3 style={{ color: '#ee5732' }}>{info.name}</h3>
+
                     <p>Price: {info.costForTwo}</p>
-                    <p>Rating: {rateStar(info.avgRating
-                    )}</p>
+                    <p>
+                        <i class="fa-solid fa-star"></i> {info.avgRating
+                        } &nbsp;&bull;&nbsp;
+                        <span>{info.sla.slaString}</span>&nbsp;&bull;&nbsp;
+                        <span>{info.sla.lastMileTravelString
+                        }</span>
+                    </p>
                     <p>{info.cuisines.join(', ')}</p>
                 </div>
                 <button id='order-btn'>Order</button>
