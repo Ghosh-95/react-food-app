bd70b9dd889285d552787d4dfaf7dfdaf48a0e39
Sushovan <sushovanghosh960@gmail.com>
2024-02-13 19:58:03 +0530

    create a customized error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,12 +1,16 @@
-import { useRouteError } from "react-router-dom"
+import { useRouteError } from "react-router-dom";
+import '../css/error.css';
 
 export default function Error() {
     const err = useRouteError();
 
     return (
-        <>
-            <h1>{err.status} {err.statusText}</h1>
-            <p>{err.data}</p>
-        </>
+        <article className="error-container">
+            <p className="error-head">Warning ⚠️</p>
+            <div className="error-body">
+                <h1>{err.status} {err.statusText}</h1>
+                <p>{err.data}</p>
+            </div>
+        </article>
     )
 }
