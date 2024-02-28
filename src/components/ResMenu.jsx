4dd4a0bc0ae8a5a48549fa7881556a45e0972d20
Sushovan <sushovanghosh960@gmail.com>
2024-02-28 14:07:12 +0530

    fix styling

--- src/components/ResMenu.jsx
+++ src/components/ResMenu.jsx
@@ -6,36 +6,25 @@ import { MenuList } from "./MenuList";
 
 export default function ResMenu() {
 
-    // https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId=278225&catalog_qa=undefined&submitAction=ENTER
-    const [resData, setResData] = useState(null);
+    const [resData, setResData] = useState([]);
 
     useEffect(() => {
         fetchMenuData();
     }, []);
 
     async function fetchMenuData() {
-        const response = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId=278225&catalog_qa=undefined&submitAction=ENTER");
+        const response = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId=8693");
 
         const { data } = await response.json();
         setResData(data);
     }
-
     const menuData = resData?.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards;
-    // const { id, name, city, areaName, costForTwoMessage
-    //     , cuisines, avgRating, totalRatingsString, feeDetails: { message }, sla: { lastMileTravelString } } = resData?.cards[2]?.card?.card?.info;
-    /**
-     * <h1>{resName}</h1>
-            <ul>
-                {menuData.map(data => {
-                    return (<li key={data.card?.info.id}>
-                        <h2>{data.card?.info.name}</h2>
-                    </li>)
-                })}
-            </ul>
-     */
 
-    /**
-     * <section className="res-details">
+    const { id, name, areaName, cuisines, avgRating, totalRatingsString, feeDetails: { message }, sla: { lastMileTravelString } } = resData.cards[2]?.card?.card?.info;
+
+    return !menuData ? <Shimmer /> : (
+        <>
+            <section className="res-details">
                 <h1>{name}</h1>
 
                 <div className="res-header">
@@ -46,37 +35,15 @@ export default function ResMenu() {
                         <p>{areaName}, {lastMileTravelString}</p>
                     </div>
 
-                    <div className="col-right"></div>
-
-                    <p className="res__header-foot">{message}</p>
-                </div>
-            </section>
-     * 
-     */
-    // let name, areaName, lastMileTravelString, cuisines = ['abc'], message;
-
-    return !menuData ? <Shimmer /> : (
-        <>
-            <section className="res-details">
-                <h1>AL Baik Fast Food Center</h1>
-
-                <div className="res-header">
-
-                    <div className="col-left">
-                        <h2>AL Baik Fast Food Center</h2>
-                        <p className="cuisines">Biryani, North Indian</p>
-                        <p>Bullyganj, 1.3 km</p>
-                    </div>
-
                     <div className="col-right">
                         <div>
-                            <span className="res-rating"><i className="fa-solid fa-star"></i>3.9</span>
-                            <span className="rating-string">1k+ ratings</span>
+                            <span className="res-rating"><i className="fa-solid fa-star"></i>{avgRating}</span>
+                            <span className="rating-string">{totalRatingsString}</span>
                         </div>
                     </div>
 
                 </div>
-                <p className="res__header-foot"><i className="fa-solid fa-motorcycle"></i>0.8 kms | ₹34 Delivery fee will apply</p>
+                <p className="res__header-foot"><i className="fa-solid fa-motorcycle"></i>{message}</p>
 
                 <ul className="menu-lists">
 
