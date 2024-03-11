e4dc2279f24c8f402849bb9a05768db650b723ab
Sushovan <sushovanghosh960@gmail.com>
2024-03-11 20:35:22 +0530

    replace About functional component with class component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,19 @@
-// import User from "./User";
 import UserClass from "./UserClass";
+import { Component } from "react";
 import "../css/about.css"
 
-export default function About() {
-    return (
-        <section className="user-about">
-            <h1>About</h1>
-            <p>This site is build with love while learning React. Design by Sushovan Ghosh</p>
-            <UserClass userData={{ name: 'Sushovan Ghosh', job: 'Frontend Developer', github: "https://github.com/Ghosh-95", api: "Swiggy API" }} />
+export default class About extends Component {
+    constructor() {
+        super();
+    }
 
-
-        </section>
-    )
+    render() {
+        return (
+            <section className="user-about">
+                <h1>About</h1>
+                <p>This site is build with love while learning React. Design by Sushovan Ghosh</p>
+                <UserClass userData={{ name: 'Sushovan Ghosh', job: 'Frontend Developer', github: "https://github.com/Ghosh-95", api: "Swiggy API" }} />
+            </section>
+        )
+    }
 }
