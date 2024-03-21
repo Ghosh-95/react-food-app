b2c70727a2f363602034c7022be35e3ac711e080
Sushovan <sushovanghosh960@gmail.com>
2024-03-21 14:25:02 +0530

    use data file to store API link

--- src/utils/custom_hooks/useRestaurants.js
+++ src/utils/custom_hooks/useRestaurants.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { REST_DATA_URL } from "../data";
 
 export default function useRestaurants() {
     const [data, setData] = useState([]);
@@ -8,7 +9,7 @@ export default function useRestaurants() {
     }, []);
 
     async function fetchData() {
-        const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+        const response = await fetch(REST_DATA_URL);
         const { data } = await response.json();
         const cardData = data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
 
--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -120,5 +120,6 @@ const dataObj = [
 
 const ASSET_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660';
 const MENU_IMAGE_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/';
-const RES_MENU_URL = 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId'
-export { dataObj, ASSET_URL, MENU_IMAGE_URL, RES_MENU_URL };
+const RES_MENU_URL = 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId';
+const REST_DATA_URL = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+export { dataObj, ASSET_URL, MENU_IMAGE_URL, RES_MENU_URL, REST_DATA_URL };
