12fd8e6ee7229fb61f67a1d08a5a9eb1145c07d0
Sushovan <sushovanghosh960@gmail.com>
2024-03-28 13:52:18 +0530

    add tailwind to components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,25 +4,24 @@ export default function RestaurantCard({ props: { info } }) {
 
     return (
         <>
-            <div className='res-cards'>
+            <div className="rounded-md w-[18.5rem] h-[21rem] overflow-hidden transition-all duration-200 ease-in-out hover:shadow-2xl hover:cursor-pointer">
                 <div>
-                    <img src={`${ASSET_URL}/${info.cloudinaryImageId}`} alt={info.cuisines.join(', ') + ' image'} className='food-image' />
+                    <img src={`${ASSET_URL}/${info.cloudinaryImageId}`} alt={info.cuisines.join(', ') + ' image'} className="w-full h-[12.5rem] object-cover rounded-t-md transition-all duration-300 hover:scale-105" />
                 </div>
 
-                <div className='card-details'>
-                    <h3 style={{ color: '#ee5732' }}>{info.name}</h3>
+                <div className="px-[0.8rem]">
+                    <h3 className='text-green-700 font-semibold mt-2 text-[1.2rem]'>{info.name}</h3>
 
                     <p>Price: {info.costForTwo}</p>
                     <p>
-                        <i className="fa-solid fa-star"></i> {info.avgRating
+                        <i className="fa-solid fa-star text-green-600 w-[1rem]"></i> {info.avgRating
                         } &nbsp;&bull;&nbsp;
                         <span>{info.sla.slaString}</span>&nbsp;&bull;&nbsp;
                         <span>{info.sla.lastMileTravelString
                         }</span>
                     </p>
-                    <p>{info.cuisines.join(', ')}</p>
+                    <p className='text-sm'>{info.cuisines.slice(0, 3).join(', ')}{info.cuisines.length > 3 ? "..." : ""}</p>
                 </div>
-                <button id='order-btn'>Order</button>
 
             </div>
         </>
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -58,25 +58,25 @@ export default function MainComponent() {
                         type="search"
                         name="search-food"
                         id="search-bar"
-                        className="w-[75%] text-black mr-[1.8rem] py-[0.6rem] px-[1.2rem] border-[1px] border-solid border-[#ee5732] outline-none rounded-md"
+                        className="sm:w-[70%] text-black mr-[1.8rem] py-[0.2rem] md:py-[0.5rem] px-[1rem] border-[1px] border-solid border-[#ee5732] outline-none rounded-md lg:w-[75%]"
                         value={inputText} />
                     <label htmlFor="search-bar"></label>
 
                     <Button
-                        variants="bg-[#ee5732]"
+                        variants="bg-[#ee5732] py-[0.18rem] md:py-[0.5rem]"
                         onClick={handleClick}>Search
                     </Button>
                 </form>
 
                 <Button
-                    variants="mt-3 mb-[4rem] ml-[8.3rem] bg-[#ee5732] font-[1rem]"
+                    variants="mt-3 md:ml-[8.3rem] ml-[4rem] bg-[#ee5732] font-[1rem] active:bg-[#963118]"
                     onClick={handleAllRestaurant}>Top Restaurants
                 </Button>
                 <Button
-                    variants="mt-3 ml-3 mb-[4rem] bg-[#ee5732] font-[1rem]"
+                    variants="mt-3 ml-3 bg-[#ee5732] font-[1rem] active:bg-[#963118]"
                     onClick={handleTopRestaurant}>All Restaurants
                 </Button>
-                <div className="res-container">
+                <div className="max-w-[75rem] my-[5rem] mx-auto flex justify-center items-center flex-wrap gap-[2rem]">
 
                     {
                         filteredObj.map(data => (<Link key={data.info.id} to={`restaurant/${data.info.id}`}>
