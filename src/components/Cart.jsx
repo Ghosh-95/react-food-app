afd96d6d6f09b42a8524f37f10cf0a688f515714
Sushovan <sushovanghosh960@gmail.com>
2024-04-12 21:13:43 +0530

    feat: implement clear cart function in the cart component
    provide useDispatch hook to get a dispatch function
    invoke the dispatch function with the clearItem action while clicking on the button

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,16 +1,26 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import CartList from './CartList';
+import Button from './Button';
+import { clearItem } from '../utils/cartSlices';
 
 export default function Cart() {
     const cartItems = useSelector(state => state.cart.item);
+
+    const dispatch = useDispatch();
+    function handleClearCartItem() {
+        dispatch(clearItem());
+    };
 
     return (
         <section className="min-h-[100vh] text-center mt-10">
             <h1 className="font-semibold text-[#fa7070]">My Cart <span className="text-green-500">({cartItems.length ? cartItems.length : 0})</span></h1>
 
-            <ul className="w-[65%] mx-auto my-10">
-                {cartItems.map(data => <CartList itemData={data} key={data.id} />)}
-            </ul>
+            <div className='my-7'>
+                {cartItems.length > 0 ? <Button variants={'bg-red-500 hover:bg-red-400 active:bg-red-600 text-white font-bold ml-4 flex'} onClick={handleClearCartItem}>Clear Cart</Button> : <p className='text-xl text-gray-500'>Your cart is empty! Please add items to cart</p>}
+                <ul className="w-[65%] m-4">
+                    {cartItems.map(data => <CartList itemData={data} key={data.id} />)}
+                </ul>
+            </div>
         </section>
     )
 }
