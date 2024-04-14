e238595d917327bb78f109557e8bb6877fca7e4a
Sushovan <sushovanghosh960@gmail.com>
2024-04-14 13:00:11 +0530

    refactor and optimized Error component and CartList component(add defaultPrice if price is not available)

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -20,7 +20,7 @@ export default function CartList({ itemData }) {
             <div className='w-[50%] flex justify-between items-center'>
                 <img className='w-[34%] rounded-full' src={`${MENU_IMAGE_URL}${itemData.imageId}`} alt={`${itemData.name} image`} />
                 <p className='w-[42%]'>{itemData.name}</p>
-                <p className='font-semibold'>₹{Math.floor(itemData.price / 100)}</p>
+                <p className='font-semibold'>₹{itemData.price ? Math.floor(itemData.price / 100) : Math.floor(itemData.defaultPrice / 100)}</p>
             </div>
             <div className="flex bg-green-400 px-2 text-xl items-center rounded-sm">
                 <CartButton>-</CartButton>
--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -6,9 +6,13 @@ export default function Error() {
     return (
         <article className="w-[30vw] my-[3rem] mx-[auto] bg-[#df5959] text-white text-center rounded-lg pb-[1rem]">
             <p className="bg-[#963838] text-center p-[0.5rem] rounded-t-lg">Warning ⚠️</p>
-            <div className="error-body">
+            {err && <div>
                 <h1>{err.status} {err.statusText}</h1>
                 <p>{err.data}</p>
+            </div>}
+            <div>
+                <h1 className="text-2xl">Something very bad happened</h1>
+                <p>Try reloading the page.</p>
             </div>
         </article>
     )
