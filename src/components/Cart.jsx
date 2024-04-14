34dcee7c51a17503299b6b0d64733ae825460189
Sushovan <sushovanghosh960@gmail.com>
2024-04-14 13:05:19 +0530

    styles: add styles to the paragrah when the cart is empty

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -16,7 +16,7 @@ export default function Cart() {
             <h1 className="font-semibold text-[#fa7070]">My Cart <span className="text-green-500">({cartItems.length ? cartItems.length : 0})</span></h1>
 
             <div className='my-7'>
-                {cartItems.length > 0 ? <Button variants={'bg-red-500 hover:bg-red-400 active:bg-red-600 text-white font-bold ml-4 flex'} onClick={handleClearCartItem}>Clear Cart</Button> : <p className='text-xl text-gray-500'>Your cart is empty! Please add items to cart</p>}
+                {cartItems.length > 0 ? <Button variants={'bg-red-500 hover:bg-red-400 active:bg-red-600 text-white font-bold ml-4 flex'} onClick={handleClearCartItem}>Clear Cart</Button> : <p className='text-xl text-red-600 w-[70%] mx-auto bg-red-300 p-2 rounded-md border-2 border-solid border-red-600'>Your cart is empty! Please add items to cart</p>}
                 <ul className="w-[65%] m-4">
                     {cartItems.map(data => <CartList itemData={data} key={data.id} />)}
                 </ul>
