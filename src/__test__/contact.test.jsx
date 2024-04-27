dbe68d263d5553dd656b35da59b653ce329f36ef
Sushovan <sushovanghosh960@gmail.com>
2024-04-27 20:38:45 +0530

    use describe and it to test component

--- src/__test__/contact.test.jsx
+++ src/__test__/contact.test.jsx
@@ -1,9 +1,10 @@
-import { describe, expect, it, test } from "vitest";
+import { describe, expect, it } from "vitest";
 import Contact from "../components/Contact";
 import { render, screen } from "@testing-library/react";
 
-test('component renders', () => {
-    render(<Contact />);
-    const heading = screen.getByRole('heading');
-    expect(heading).toBeInTheDocument();
+describe('Contact', () => {
+    it('renders contact component', () => {
+        render(<Contact />);
+        expect(screen.getByRole('heading')).toHaveTextContent('Contact Us');
+    })
 })
